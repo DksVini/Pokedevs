9d5072ff0d2c050c9cb44f26cd96d35b998268f9
DksVini <Hardlinebullseye@gmail.com>
2024-10-13 15:07:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const listaSelecaoPokedevs = document.querySelectorAll('.pokedev');
+listaSelecaoPokedevs.forEach(pokedev => {
+   pokedev.addEventListener('click', () => {
+
+      esconderCartaoPokedev()
+
+      const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+
+      desativarPokedevNaListagem();
+
+      ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+
+   })
+})
+
+function esconderCartaoPokedev() {
+   const cartaoPokedevAberto = document.querySelector('.aberto');
+   cartaoPokedevAberto.classList.remove('aberto');
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+   const idPokedevSelecionado = pokedev.attributes.id.value;
+   const idDoCartaoPokedevParaAbrir = 'cartao-' + idPokedevSelecionado;
+   const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+   cartaoPokedevParaAbrir.classList.add('aberto');
+   return idPokedevSelecionado;
+}
+
+function desativarPokedevNaListagem() {
+   const pokedevAtivoNaListagem = document.querySelector('.ativo');
+   pokedevAtivoNaListagem.classList.remove('ativo');
+}
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+   const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+   pokedevSelecionadoNaListagem.classList.add('ativo');
+}
+
